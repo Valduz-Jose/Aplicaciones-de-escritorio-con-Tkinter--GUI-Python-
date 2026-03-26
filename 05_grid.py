1e2b5e74afff5621467060efd6685a1ad56bfc06
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-26 11:56:17 -0400

    Add Tkinter window with button grid layout implementation

---
+++ 05_grid.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import ttk #es una mejora de tkinter que tiene componentes con un diseño más moderno y atractivo
+
+ventana = tk.Tk()
+ventana.geometry("600x400")
+ventana.title("Nueva Ventana")
+ventana.config(background='#1d2d44')
+
+# Manejo de grid
+boton1 = ttk.Button(ventana, text='Boton 1')
+boton2 = ttk.Button(ventana, text='Boton 2')
+boton3 = ttk.Button(ventana, text='Boton 3')
+
+# Publicados d manera horizontal
+# boton1.grid(row=0, column=0)
+# boton2.grid(row=0, column=1)
+# boton3.grid(row=0, column=2)
+
+# Publicados de manera vertical
+# boton1.grid(row=0, column=0)
+# boton2.grid(row=1, column=0)
+# boton3.grid(row=2, column=0)
+
+# Publicados en diagonal
+boton1.grid(row=0, column=0)
+boton2.grid(row=1, column=1)
+boton3.grid(row=2, column=2)
+
+
+ventana.mainloop()
